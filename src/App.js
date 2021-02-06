a96eda8539846a2ecdfe7a8019d578057f591ee2
Nick Snyder <nick.snyder@airhydropower.com>
2021-02-06 15:36:33 -0500

    geolocaiton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [cords, setCords] = useState();
+
+  const geo = () => {
+    navigator.geolocation.getCurrentPosition(geoSuccess);
+  };
+
+  const geoSuccess = (pos) => {
+    var crd = pos.coords;
+
+    console.log("Your current position is:");
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+
+    setCords(pos.cords);
+  };
+
+  useEffect(() => {
+    geo();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
