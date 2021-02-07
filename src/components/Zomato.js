eeef7b425e683156c89be54930d28fe952052815
Miranda Burbridge <mirandaburbridge@gmail.com>
2021-02-07 15:37:24 -0600

    error free at least

--- src/components/Zomato.js
+++ src/components/Zomato.js
@@ -6,12 +6,15 @@ import './Zomato.css';
 
 
 
-const Zomato = ({ props }) => {
+const Zomato = (props) => {
+    const [latitude, setLatitude] = useState();
+    const [longitude, setLongitude] = useState();
+    const [zomato, setZomato] = useState();
 
     // const baseUrl = `https://developers.zomato.com/api/v2.1/geocode`;
     // const key = 'f0b8f4f1370b6c9eb8cc12e061b6c06f';
 
-    const [cords, setCords] = useState();
+
     const geo = () => {
         navigator.geolocation.getCurrentPosition(geoSuccess);
     };
@@ -20,42 +23,33 @@ const Zomato = ({ props }) => {
         console.log("Your current position is:");
         console.log(`Latitude : ${pos.coords.latitude}`);
         console.log(`Longitude: ${pos.coords.longitude}`);
-        setCords(pos.cords);
+        setLatitude(pos.coords.latitude);
+        setLongitude(pos.coords.longitude);
+
     };
     useEffect(() => {
         geo();
-    }, []);
-
-    const fetchResults = () => {
-        const url = `https://developers.zomato.com/api/v2.1/geocode?lat=${setCords.latitude}&lon=${setCords.longitude}`;
-        console.log(url);
-        const response = fetch(url, {
+        let url = `https://developers.zomato.com/api/v2.1/geocode?lat=${latitude}&lon=${longitude}`
+        fetch(url, {
             headers: {
                 Accept: "application/json",
                 "User-Key": "f0b8f4f1370b6c9eb8cc12e061b6c06f"
             }
-                .then(res => res.json())
-                .then((data) => {
-                    this.setState({ 'zomato': data })
-                })
-                .catch(console.log)
-        });
+        })
+            .then(res => res.json())
+            .then(json => console.log(json))
+            .then(json => setZomato(json.nearby_restaurants))
+            .then(json => console.log(zomato))
+            .catch(console.log)
+    }, []);
 
-    };
-
-
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        fetchResults();
-    };
 
     return (
-        <div>
+        <div className="zom-body">
             <h1>Restaurants near you!</h1>
             <div className="restaurant-card">
 
-                <p>{ }</p>
+                <p>{zomato}</p>
 
             </div>
 
