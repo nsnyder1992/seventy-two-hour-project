ddb2e64da6c06a9241cb82c25c1fbe2a58662963
Nick Snyder <nsnyder1992@gmail.com>
2021-02-07 22:19:59 -0500

    added nav and reactstraps

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 //css
 import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 //components
-import LocationBody from "./site/LocationBody";
+import SideNav from "./site/SideNav";
 
 function App() {
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+
+  //get location
+  const geo = () => {
+    navigator.geolocation.getCurrentPosition(geoSuccess);
+  };
+
+  const geoSuccess = (pos) => {
+    var crd = pos.coords;
+    setLatitude(Math.floor(crd.latitude * 100) / 100); //rounded to two decimal places
+    setLongitude(Math.floor(crd.longitude * 100) / 100); //rounded to two decimal places
+  };
+
+  useEffect(() => {
+    geo();
+  }, []);
+
   return (
     <div className="App">
-      <LocationBody />
+      <Router>
+        <SideNav latitude={latitude} longitude={longitude} />
+      </Router>
     </div>
   );
 }
