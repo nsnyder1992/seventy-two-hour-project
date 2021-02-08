c0b932233ded74ae44dd6c908c6eb923635575eb
Nick Snyder <nsnyder1992@gmail.com>
2021-02-07 19:05:10 -0500

    Refactored components

--- src/App.js
+++ src/App.js
@@ -5,10 +5,32 @@ import Zomato from "./components/Zomato";
 import NASA from "./components/NASA";
 
 function App() {
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+
+  //get location
+  const geo = () => {
+    navigator.geolocation.getCurrentPosition(geoSuccess);
+  };
+
+  const geoSuccess = (pos) => {
+    var crd = pos.coords;
+
+    console.log("Your current position is:");
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+    setLatitude(Math.floor(crd.latitude * 10) / 10);
+    setLongitude(Math.floor(crd.longitude * 10) / 10);
+  };
+
+  useEffect(() => {
+    geo();
+  }, []);
   return (
     <div className="App">
-      <Zomato />
-      <NASA />
+      <Zomato latitude={latitude} longitude={longitude} />
+      <NASA latitude={latitude} longitude={longitude} />
     </div>
   );
 }
--- src/components/Zomato.js
+++ src/components/Zomato.js
@@ -1,50 +1,36 @@
-import React from "react";
-// import { Link } from 'react-router-dom';
-import { useEffect, useState } from "react";
-// import App from './../App';
+import React, { useEffect, useState } from "react";
+
 import "./Zomato.css";
 
-const Zomato = (props) => {
-  const [latitude, setLatitude] = useState();
-  const [longitude, setLongitude] = useState();
-  const [zomato, setZomato] = useState();
+const Zomato = ({ latitude, longitude }) => {
+  const [zomato, setZomato] = useState([]);
 
-  // const baseUrl = `https://developers.zomato.com/api/v2.1/geocode`;
-  // const key = 'f0b8f4f1370b6c9eb8cc12e061b6c06f';
-
-  const geo = () => {
-    navigator.geolocation.getCurrentPosition(geoSuccess);
+  const handleSubmit = () => {
+    if (latitude !== undefined && longitude !== undefined) {
+      let url = `https://cors-anywhere.herokuapp.com/http://developers.zomato.com/api/v2.1/geocode?lat=${latitude}&lon=${longitude}`;
+      let tempZomato = [];
+      fetch(url, {
+        headers: {
+          Accept: "application/json",
+          "User-Key": "f0b8f4f1370b6c9eb8cc12e061b6c06f",
+        },
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          json.nearby_restaurants.map((restaurant) =>
+            tempZomato.push(restaurant)
+          );
+          setZomato(tempZomato);
+          console.log(zomato);
+        })
+        .catch(console.log);
+    }
   };
-  const geoSuccess = (pos) => {
-    // const crd = pos.coords;
-    console.log("Your current position is:");
-    console.log(`Latitude : ${pos.coords.latitude}`);
-    console.log(`Longitude: ${pos.coords.longitude}`);
-    setLatitude(pos.coords.latitude);
-    setLongitude(pos.coords.longitude);
-  };
-  useEffect(() => {
-    geo();
-    let url = `https://developers.zomato.com/api/v2.1/geocode?lat=${latitude}&lon=${longitude}`;
-    fetch(url, {
-      headers: {
-        Accept: "application/json",
-        "User-Key": "f0b8f4f1370b6c9eb8cc12e061b6c06f",
-      },
-    })
-      .then((res) => res.json())
-      .then((json) => console.log(json))
-      .then((json) => setZomato(json.nearby_restaurants))
-      .then((json) => console.log(zomato))
-      .catch(console.log);
-  }, []);
 
   return (
     <div className="zom-body">
       <h1>Restaurants near you!</h1>
-      <div className="restaurant-card">
-        <p>{zomato}</p>
-      </div>
+      <button onClick={handleSubmit}>Try it out!</button>
     </div>
   );
 };
